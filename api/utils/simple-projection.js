3b5a0bb7215fb9630b55a4252cb47772baf3e0c5
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-02-21 17:34:50 -0800

    adds mongodriver and plugs it in to first sample graphql query/resolver

---
+++ api/utils/simple-projection.js
@@ -0,0 +1,19 @@
+const R = require('ramda');
+
+module.exports = function(info) {
+  // path to deeply nested property of graphql tree
+  const sel_path = R.lensPath([
+    'operation',
+    'selectionSet',
+    'selections',
+    '0',
+    'selectionSet',
+    'selections'
+  ]);
+  
+  const projection = R.pipe(
+    R.view(sel_path),
+    R.map(selection => ({[ selection.name.value ]: 1 })),
+    R.mergeAll
+  )(info);
+}
