82c619d5850ae85992292bdafedb3ff15c585d7c
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-03-01 18:55:18 -0800

    adds automatic generation of user data

--- data/mock/mongoCreator.js
+++ data/mock/mongoCreator.js
@@ -1,6 +1,55 @@
 const bcrypt = require('bcrypt');
 const fs = require('fs');
 const util = require('util');
+const faker = require('faker');
+const uuid = require('uuid/v4');
+
+const USER_NUM = 1000;
+const GENDER = ['male', 'female', 'other'];
+
+async function createUsers() {
+  let users = [];
+
+  for (let i = 0; i <= USER_NUM; i++) {
+    users.push({
+      _id: uuid(),
+      email: faker.internet.email().toLowerCase(),
+      password: faker.internet.password()
+    });
+
+  }
+
+  fs.writeFileSync(`${__dirname}/users-login-data.json`, JSON.stringify(users, null, '  '));
+
+  users = users.map(async (user, ix) => {
+    const friends = (ix < USER_NUM / 2 ? users.slice(ix + 1, ix + 10) : users.slice(ix + 1, ix + 10)).map(user => user._id);
+
+    const new_user = Object.assign(user, {
+      password: await bcrypt.hash(user.password, 10),
+      createdAt: +(faker.date.past()),
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      friends,
+      phone_number: faker.phone.phoneNumber(),
+      gender: GENDER[Math.round(Math.random() * GENDER.length)]
+    });
+
+    process.stdout.write('.');
+
+    return new_user;
+  });
+  users = JSON.stringify(await Promise.all(users), null, ' ');
+
+  const file_output = `
+db.users.insertMany(
+  ${users},
+  { ordered: false }
+);`;
+
+  fs.writeFileSync(`${__dirname}/mongo/users-social.js`, file_output);
+}
+
+
 
 // const readFileSync = util.promisify(fs.readFileSync);
 
@@ -38,6 +87,7 @@ db.users.insertMany(
   console.log(new_users);
 }
 
-transformUsers();
+// transformUsers();
+createUsers();
 
 
