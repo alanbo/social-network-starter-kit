6d8ede13a1bc188e949cf59691249245cd8b739f
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-03-22 20:47:44 -0700

    hooks up basic user posts

--- view/src/redux/reducers/userPostsReducer.ts
+++ view/src/redux/reducers/userPostsReducer.ts
@@ -1,3 +1,7 @@
+import { UserQuery_user_posts } from '../../graphql/operation-result-types';
+import { DeepReadonly } from 'utility-types';
+import { UserActions } from '../actions/userActions'
+
 import {
   get_user_with_posts,
   login,
@@ -5,10 +9,22 @@ import {
 } from '../actions/types';
 
 
-export default function (state = {}, action: any) {
+export type UserPostsState = DeepReadonly<UserQuery_user_posts>[] | null;
+
+export default function (state: UserPostsState = [], action: UserActions) {
   switch (action.type) {
     case get_user_with_posts:
-      return state;
+    case login:
+      const user = action.payload.user;
+
+      if (!user) {
+        return state;
+      }
+
+      return user.posts;
+
+    case logout:
+      return [];
 
     default:
       return state
--- view/src/screens/Dashboard/index.tsx
+++ view/src/screens/Dashboard/index.tsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { AppState } from '../../redux/reducers';
+import { UserPostsState } from '../../redux/reducers/userPostsReducer';
 
-export default class Dashboard extends Component {
+interface Props {
+  posts: UserPostsState;
+}
+
+class Dashboard extends Component<Props> {
   render() {
+    const { posts } = this.props;
+
     return (
       <div>
         <h1>Dashboard</h1>
+
+        {
+          posts && posts.map(post => (
+            <p>{post.message}</p>
+          ))
+        }
 
       </div>
     );
   }
 };
+
+function mapStateToProps(state: AppState) {
+  return {
+    posts: state.user_posts
+  }
+}
+
+
+
+export default connect(mapStateToProps)(Dashboard);
