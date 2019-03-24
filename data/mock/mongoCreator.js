c09599c1e94ef250f57f03ed8cc63ace048b4dd0
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-03-23 18:04:01 -0700

    updates data so that friends and friend request are consitant according to set convention.

--- data/mock/mongoCreator.js
+++ data/mock/mongoCreator.js
@@ -4,35 +4,62 @@ const util = require('util');
 const faker = require('faker');
 const uuid = require('uuid/v4');
 
-const USER_NUM = 1000;
+// const USER_NUM = 100;
 const POSTS_NUM = 10;
 const GENDER = ['male', 'female', 'other'];
 
 async function createUsers() {
-  let users = [];
+  // let users = [];
 
-  for (let i = 0; i <= USER_NUM; i++) {
-    users.push({
-      _id: uuid(),
-      email: faker.internet.email().toLowerCase(),
-      password: faker.internet.password()
-    });
+  // for (let i = 0; i <= USER_NUM; i++) {
+  //   users.push({
+  //     _id: uuid(),
+  //     email: faker.internet.email().toLowerCase(),
+  //     password: faker.internet.password()
+  //   });
 
-  }
+  // }
 
-  fs.writeFileSync(`${__dirname}/users-login-data.json`, JSON.stringify(users, null, '  '));
+  // fs.writeFileSync(`${__dirname}/users-login-data.json`, JSON.stringify(users, null, '  '));
+  let users = JSON.parse(fs.readFileSync(`${__dirname}/users-login-data.json`).toString());
 
   console.log('Creating users: ');
 
   users = users.map(async (user, ix) => {
     // For the first half of the users add the next 10 users as friends.
     // For the second half ot the users add the previous 10 users as friends.
-    const friends = (ix < USER_NUM / 2 ? users.slice(ix + 1, ix + 10) : users.slice(ix - 10, ix - 1)).map(user => user._id);
+    // const friends = (ix < USER_NUM / 2 ? users.slice(ix + 1, ix + 10) : users.slice(ix - 10, ix - 1)).map(user => user._id);
 
-    // TO DO: friends relationship needs to be mutual. Add the user id to the friend's friend list. 
+    let friends;
+    let friend_requests;
 
-    // Same as above but for the next 5 users that are not already friends. 
-    const friend_requests = (ix < USER_NUM / 2 ? users.slice(ix + 11, ix + 16) : users.slice(ix - 16, ix - 11)).map(user => user._id);
+    if (ix < 6) {
+      friends = users.slice(0, ix + 6);
+      // adds next 5 users as friends.
+    } else if (ix < users.length - 6) {
+      friends = users.slice(ix - 5, ix + 6);
+      // if there is less then 5 more users adds all the remaining users.
+    } else {
+      const remaining_num = users.length - ix;
+
+      friends = users.slice(ix - 5, ix + remaining_num);
+    }
+
+    // make sure only ids are in the array and remove the user from its friends.
+    friends = friends.map(friend => friend._id).filter(id => !(id === user._id));
+
+    // adds next 5 after 5 users as friend requests.
+    if (ix < users.length - 11) {
+      friend_requests = users.slice(ix + 6, ix + 11);
+      // if there is less then 5 + 5 more users adds all the remaining users after 5 next ones.
+    } else {
+      const remaining_num = users.length - ix;
+
+      friend_requests = users.slice(ix + 6, ix + remaining_num);
+    }
+
+    friend_requests = friend_requests.map(user => user._id);
+
 
     const new_user = Object.assign(user, {
       password: await bcrypt.hash(user.password, 10),
