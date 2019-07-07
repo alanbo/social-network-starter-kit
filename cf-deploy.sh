04b26cd66de357889b9b3bf4c628d72117a19d12
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-07-06 17:45:47 -0700

    updates deploy stack to produce env file, creates update stack script that uses that file

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -2,15 +2,19 @@
 
 echo "Provide Arn of dhub_password in SSM:"
 read dhub_password_arn
+echo "export dhub_password_arn=\"$dhub_password_arn\"" >> .cf-env
 
 echo "Provide docker user name:"
 read docker_user_name
+echo "export docker_user_name=\"$docker_user_name\"" >> .cf-env
 
 echo "Provide docker repo name:"
 read dhub_repo_name
+echo "export dhub_repo_name=\"$dhub_repo_name\"" >> .cf-env
 
 echo "Provide github repo url:"
 read github_repo_name
+echo "export github_repo_name=\"$github_repo_name\"" >> .cf-env
 
 aws cloudformation create-stack \
   --stack-name social-network-starter-kit \
