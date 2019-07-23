ff9f9eb9436a7978d8ceb9c7b748175fd9fef76e
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-07-22 20:17:21 -0700

    adds basic configuration to cloudform template

---
+++ devops/parts/ecs-ami-mappings.ts
@@ -0,0 +1,60 @@
+
+export default {
+  'us-east-2': {
+    imageId: 'ami-0dca97e7cde7be3d5'
+  },
+  'us-east-1': {
+    imageId: 'ami-0fac5486e4cff37f4'
+  },
+  'us-west-1': {
+    imageId: 'ami-0c6e63b58aac1048e'
+  },
+  'us-west-2': {
+    imageId: 'ami-0e5e051fd0b505db6'
+  },
+  'ap-east-1': {
+    imageId: 'ami-02252d984c7e3595d'
+  },
+  'ap-northeast-1': {
+    imageId: 'ami-04a735b489d2a0320'
+  },
+  'ap-northeast-2': {
+    imageId: 'ami-0accbb5aa909be7bf'
+  },
+  'ap-south-1': {
+    imageId: 'ami-0a8bf4e187339e2c1'
+  },
+  'ap-southeast-1': {
+    imageId: 'ami-05c6d22d98f97471c'
+  },
+  'ap-southeast-2': {
+    imageId: 'ami-039bb4c3a7946ce19'
+  },
+  'ca-central-1': {
+    imageId: 'ami-01c07ee95e77abba8'
+  },
+  'eu-central-1': {
+    imageId: 'ami-0650e7d86452db33b'
+  },
+  'eu-north-1': {
+    imageId: 'ami-059aa04f0c253ad6b'
+  },
+  'eu-west-1': {
+    imageId: 'ami-0ae254c8a2d3346a7'
+  },
+  'eu-west-2': {
+    imageId: 'ami-0de1dc478496a9e9b'
+  },
+  'eu-west-3': {
+    imageId: 'ami-0d260f3e5ccd06043'
+  },
+  'sa-east-1': {
+    imageId: 'ami-038707d64e5b8e7ba'
+  },
+  'us-gov-east-1': {
+    imageId: 'ami-0a224902b35f8ad6c'
+  },
+  'us-gov-west-1': {
+    imageId: 'ami-04c68165'
+  }
+};
--- devops/template.ts
+++ devops/template.ts
@@ -1,5 +1,28 @@
-import cloudform, { Fn, Refs, EC2, StringParameter, ResourceTag } from "cloudform";
+import cloudform, { Fn, Refs, EC2, ECS, StringParameter, ResourceTag } from "cloudform";
+import ecsAmiMappings from './parts/ecs-ami-mappings';
+const STACK_NAME = 'social-network-starter-kit';
 
 export default cloudform({
+  Mappings: {
+    amiIDS: ecsAmiMappings
+  },
+  Resources: {
+    StagingECSCluster: new ECS.Cluster({
+      ClusterName: `${STACK_NAME}-ecs-cluster`
+    }),
+
+    StagingECSTaskDef: new ECS.TaskDefinition({
+
+    }),
+
+    StagingECSService: new ECS.Service({
+      Cluster: Fn.Ref('StagingECSCluster'),
+      TaskDefinition: Fn.Ref('StagingECSTaskDef')
+    }),
+
+    StagingEC2Instance: new EC2.Instance({
+      ImageId: Fn.FindInMap('amiIDS', Refs.Region, 'imageId')
+    })
+  }
 
 });
