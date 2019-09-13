18384b387d2d080588b34097f0b74ac934a8700c
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-09-12 18:17:10 -0700

    fixes storybook for navigation frame

--- view/src/stories/data/navigation-frame.data.ts
+++ view/src/stories/data/navigation-frame.data.ts
@@ -1,11 +1,13 @@
-import { UserQuery_user } from '../../graphql/operation-result-types';
+import { User } from '../../apollo/client-schema';
 
-export const user: UserQuery_user = {
-  "_id": "28397f52-2048-4646-a180-016e6415d91e",
-  "first_name": "Ryann",
-  "last_name": "Bahringer",
-  "email": "crystel_sawayn51@gmail.com",
-  gender: 'male',
-  createdAt: new Date('2017-06-06T17:50:31.731Z'),
-  phone_number: '19992228888'
+export const user: User = {
+  _id: '61dbb80e-a681-49e2-89c2-e01ae7c96439',
+  birthdate: null,
+  email: 'something@gmail.com',
+  email_verified: true,
+  family_name: 'MrTry',
+  gender: null,
+  given_name: 'Chris',
+  nickname: 'MrRandom',
+  phone_number: null,
 };
--- view/src/stories/navigation-frame.stories.tsx
+++ view/src/stories/navigation-frame.stories.tsx
@@ -4,15 +4,21 @@ import { action } from '@storybook/addon-actions';
 import { withKnobs, boolean, object } from '@storybook/addon-knobs';
 import NavigationFrame from '../components/NavigationFrame';
 import { user } from './data/navigation-frame.data';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({});
+
 
 storiesOf('NavigationFrame', module)
   .addDecorator(withKnobs)
-  .add('standard', () => (<div></div>
-    // <NavigationFrame
-    //   user={object('User', user)}
-    //   signOut={action('Sign Out')}
-    //   loading={boolean('Is Loading', false)}
-    // >
-    //   <div>NavigationFrame frame child</div>
-    // </NavigationFrame>
+  .addDecorator(getNavigation => (<ThemeProvider theme={theme}>{getNavigation()}</ThemeProvider>))
+  .add('standard', () => (
+    <NavigationFrame
+      user={object('User', user)}
+      signOut={action('Sign Out')}
+    // loading={boolean('Is Loading', false)}
+    >
+      <div>NavigationFrame frame child</div>
+    </NavigationFrame>
   ));
