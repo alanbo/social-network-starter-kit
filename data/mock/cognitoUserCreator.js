bce59fdfe382450d391da912c5d28b84d21be4e7
Alan Bogusiewicz <abogusiewicz@gmail.com>
2019-09-17 14:42:34 -0700

    adds further refinements to cognitoUserCreator function

--- data/mock/cognitoUserCreator.js
+++ data/mock/cognitoUserCreator.js
@@ -3,23 +3,14 @@ const AWS = require('aws-sdk');
 const util = require('util');
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
-function promisify(func) {
-  return params => new Promise((resolve, reject) => {
-    func(params, function (err, data) {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
-}
-
 const adminCreateUser = util.promisify(cognito.adminCreateUser.bind(cognito));
 const adminInitiateAuth = util.promisify(cognito.adminInitiateAuth.bind(cognito));
 const respondToAuthChallenge = util.promisify(cognito.respondToAuthChallenge.bind(cognito));
 
 
-async function createUser(attributes, password) {
+export default async function createUser(attributes, password) {
   var params = {
-    UserPoolId: process.env.USER_POOL_ID, /* required */
+    UserPoolId: process.env.USER_POOL_ID,
     Username: attributes.email,
     DesiredDeliveryMediums: ['EMAIL'],
     ForceAliasCreation: false,
@@ -36,7 +27,7 @@ async function createUser(attributes, password) {
   const auth_params = {
     AuthFlow: 'ADMIN_NO_SRP_AUTH',
     ClientId: process.env.CLIENT_ID,
-    UserPoolId: process.env.USER_POOL_ID, /* required */
+    UserPoolId: process.env.USER_POOL_ID,
     AuthParameters:
     {
       USERNAME: params.Username,
@@ -44,48 +35,45 @@ async function createUser(attributes, password) {
     }
   };
 
-  let user_data;
-  let auth_data;
-
   try {
-    user_data = await adminCreateUser(params);
-    auth_data = await adminInitiateAuth(auth_params);
+    // create a user
+    const user_data = await adminCreateUser(params);
+
+    // authenticate user with temporary password
+    const auth_data = await adminInitiateAuth(auth_params);
+
+    const challenge_params = {
+      ChallengeName: 'NEW_PASSWORD_REQUIRED',
+      ClientId: process.env.CLIENT_ID,
+      ChallengeResponses: {
+        USERNAME: params.Username,
+        NEW_PASSWORD: password
+      },
+      Session: auth_data.Session
+
+    };
+
+    // verify users email
+    // in cognito user has to change the password after the first login
+    await respondToAuthChallenge(challenge_params);
+
+    const user = {};
+
+    // convert the attributes to object-map format
+    user_data.User.Attributes.forEach(attr => {
+      user[attr.Name] = attr.Value;
+    });
+
+    return user;
   } catch (e) {
-    console.log(e);
+    console.error(e);
+    throw e;
   }
-
-  const challenge_params = {
-    ChallengeName: 'NEW_PASSWORD_REQUIRED',
-    ClientId: process.env.CLIENT_ID,
-    ChallengeResponses: {
-      USERNAME: params.Username,
-      NEW_PASSWORD: password
-    },
-    Session: auth_data.Session
-  };
-
-  try {
-    const challenge_data = await respondToAuthChallenge(challenge_params);
-    console.log('challenge', challenge_data);
-
-  } catch (e) {
-    console.log(e);
-  }
-
-  cognito.respondToAuthChallenge(params, function (err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else console.log(data);           // successful response
-  });
-
-  console.log(auth_data);
-  console.log(user_data);
-  return user_data;
-
 }
 
-createUser({
-  email: 'rrr@whatever.com',
-  gender: 'male',
-  given_name: 'alan',
-  family_name: 'bogusiewicz'
-}, '44$$Ald88$dd').then(console.log);
+// createUser({
+//   email: 'rrr8@whatever.com',
+//   gender: 'male',
+//   given_name: 'alan',
+//   family_name: 'bogusiewicz'
+// }, '44$$Ald88$dd').then(console.log);
